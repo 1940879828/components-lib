cf1f799439cf484c413c2596c1dc9be7fb58f6dc
YeJieChen <1940879828@qq.com>
2025-01-03 10:32:58 +0800

    feat: 添加主题控制组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css"
 import { useEffect } from "react"
 import { themeChange } from "theme-change"
-import Button from "./components/Button";
+import Button from "./components/Button"
+import ThemeController from "./components/ThemeController"
 
 function App() {
   useEffect(() => {
@@ -11,14 +12,32 @@ function App() {
   return (
     <div className="p-2">
       <div className="flex gap-2">
-        <Button variant="neutral" shape="square" size="lg" className="text-sm text-primary">neutral</Button>
-        <Button variant="primary" shape="circle" size="lg" className="text-sm">primary</Button>
-        <Button variant="secondary" size="xs">secondary</Button>
-        <Button variant="accent" size="sm">accent</Button>
-        <Button variant="success" size="md">success</Button>
-        <Button variant="info" size="lg">info</Button>
+        <ThemeController className="w-8 h-8" />
+        <Button
+          variant="neutral"
+          shape="square"
+          size="lg"
+          className="text-sm text-primary"
+        >
+          neutral
+        </Button>
+        <Button variant="primary" shape="circle" size="lg" className="text-sm">
+          primary
+        </Button>
+        <Button variant="secondary" size="xs">
+          secondary
+        </Button>
+        <Button variant="accent" size="sm">
+          accent
+        </Button>
+        <Button variant="success" size="md">
+          success
+        </Button>
+        <Button variant="info" size="lg">
+          info
+        </Button>
         <Button variant="warning">warning</Button>
-        <Button variant="ghost" >ghost</Button>
+        <Button variant="ghost">ghost</Button>
         <Button variant="error">error</Button>
       </div>
     </div>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
-import React, {ReactNode} from "react";
-import { cva, type VariantProps } from "class-variance-authority"
-import {cn} from "../../lib/utils.ts";
+import { type VariantProps, cva } from "class-variance-authority"
+import React, { type ReactNode } from "react"
+import { cn } from "../../lib/utils.ts"
 
 interface Props
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -8,48 +8,51 @@ interface Props
   children?: ReactNode
 }
 
-const buttonVariants = cva(
-  "btn",
-  {
-    variants: {
-      variant: {
-        neutral:"btn-neutral",
-        primary:"btn-primary",
-        secondary:"btn-secondary",
-        accent:"btn-accent",
-        ghost:"btn-ghost",
-        info:"btn-info",
-        success:"btn-success",
-        warning:"btn-warning",
-        error:"btn-error",
-      },
-      size: {
-        xs:"btn-xs",
-        sm:"btn-sm",
-        md:"btn-md",
-        lg:"btn-lg"
-      },
-      shape: {
-        default: "",
-        square: "btn-square",
-        circle: "btn-circle"
-      }
+const buttonVariants = cva("btn", {
+  variants: {
+    variant: {
+      neutral: "btn-neutral",
+      primary: "btn-primary",
+      secondary: "btn-secondary",
+      accent: "btn-accent",
+      ghost: "btn-ghost",
+      info: "btn-info",
+      success: "btn-success",
+      warning: "btn-warning",
+      error: "btn-error"
     },
-    defaultVariants: {
-      variant: "primary",
-      size: "md",
-      shape: "default"
+    size: {
+      xs: "btn-xs",
+      sm: "btn-sm",
+      md: "btn-md",
+      lg: "btn-lg"
     },
+    shape: {
+      default: "",
+      square: "btn-square",
+      circle: "btn-circle"
+    }
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "md",
+    shape: "default"
+  }
+})
+
+const Button: React.FC<Props> = React.forwardRef<HTMLButtonElement, Props>(
+  ({ children, className, variant, size, shape, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, shape, className }))}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    )
   }
 )
 
-const Button: React.FC<Props> = React.forwardRef<HTMLButtonElement, Props>(({ children, className, variant, size, shape, ...props }, ref) => {
-  return (
-    <button className={cn(buttonVariants({ variant, size, shape, className }))} ref={ref} {...props}>
-      {children}
-    </button>
-  );
-});
-
 Button.displayName = "Button"
-export default Button;
+export default Button
