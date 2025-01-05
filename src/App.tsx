3525206b8875cd5a68c8635c57d9bfc595c26b79
yejiechen <1940879828@qq.com>
2025-01-05 13:34:20 +0800

    feat: 添加dropdown组件

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import "./App.css"
 import { useEffect } from "react"
 import { themeChange } from "theme-change"
 import Button from "./components/Button"
+import DropDown, {
+  DropdownContent,
+  DropdownTrigger
+} from "./components/Popover"
 import ThemeController from "./components/ThemeController"
-import Popover from "./components/Popover";
 
 function App() {
   useEffect(() => {
@@ -41,8 +44,72 @@ function App() {
         <Button variant="ghost">ghost</Button>
         <Button variant="error">error</Button>
       </div>
-      <div className="flex gap-2">
-        <Popover/>
+      <div className="flex gap-2 pl-20 pt-20">
+        <DropDown variant="top">
+          <DropdownTrigger>
+            <Button>top</Button>
+          </DropdownTrigger>
+          <DropdownContent>
+            <li>
+              <p>Item 1</p>
+            </li>
+            <li>
+              <p>Item 2</p>
+            </li>
+          </DropdownContent>
+        </DropDown>
+        <DropDown variant="left">
+          <DropdownTrigger>
+            <Button>left</Button>
+          </DropdownTrigger>
+          <DropdownContent>
+            <li>
+              <p>Item 1</p>
+            </li>
+            <li>
+              <p>Item 2</p>
+            </li>
+          </DropdownContent>
+        </DropDown>
+        <DropDown variant="right">
+          <DropdownTrigger>
+            <Button>right</Button>
+          </DropdownTrigger>
+          <DropdownContent>
+            <li>
+              <p>Item 1</p>
+            </li>
+            <li>
+              <p>Item 2</p>
+            </li>
+          </DropdownContent>
+        </DropDown>
+        <DropDown variant="bottomEnd">
+          <DropdownTrigger>
+            <Button>bottomEnd</Button>
+          </DropdownTrigger>
+          <DropdownContent>
+            <li>
+              <p>Item 1</p>
+            </li>
+            <li>
+              <p>Item 2</p>
+            </li>
+          </DropdownContent>
+        </DropDown>
+        <DropDown variant="bottom" trigger="hover">
+          <DropdownTrigger>
+            <Button>bottom hover</Button>
+          </DropdownTrigger>
+          <DropdownContent>
+            <li>
+              <p>Item 1</p>
+            </li>
+            <li>
+              <p>Item 2</p>
+            </li>
+          </DropdownContent>
+        </DropDown>
       </div>
     </div>
   )
--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -1,15 +1,126 @@
+import { type VariantProps, cva } from "class-variance-authority"
+import type React from "react"
+import {
+  type ReactNode,
+  type RefObject,
+  createContext,
+  useContext,
+  useRef
+} from "react"
+import { cn } from "../../lib/utils.ts"
+import ClickAwayListener from "../ClickAwayListener"
 
+const DropdownContext = createContext<RefObject<HTMLDetailsElement> | null>(
+  null
+)
 
-const Popover = () => {
+interface DropdownTriggerProps {
+  children: ReactNode
+  className?: string
+}
+
+export const DropdownTrigger: React.FC<DropdownTriggerProps> = (props) => {
+  const { children, className } = props
+  const detailsRef = useContext(DropdownContext)
+
+  if (!detailsRef) {
+    throw new Error("DropdownTrigger must be used within a Popover")
+  }
+
+  const onBoxClick = () => {
+    detailsRef.current?.toggleAttribute("open")
+  }
+
   return (
-    <details className="dropdown" open>
-      <summary className="btn m-1" >open or close</summary>
-      <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-      </ul>
-    </details>
-  );
-};
+    <summary
+      className={cn("appearance-none list-none", className)}
+      onClick={onBoxClick}
+    >
+      {children}
+    </summary>
+  )
+}
 
-export default Popover;
+export const DropdownContent: React.FC<{
+  children: ReactNode
+  className?: string
+}> = ({ children, className }) => {
+  const detailsRef = useContext(DropdownContext)
+
+  if (!detailsRef) {
+    throw new Error("DropdownContent must be used within a Popover")
+  }
+
+  return (
+    <ul
+      className={cn(
+        "menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow",
+        className
+      )}
+    >
+      {children}
+    </ul>
+  )
+}
+
+interface dropdownProps
+  extends React.ButtonHTMLAttributes<HTMLDetailsElement>,
+    VariantProps<typeof dropdownVariants> {
+  children: React.ReactNode
+  trigger?: "click" | "hover"
+}
+
+const dropdownVariants = cva("dropdown", {
+  variants: {
+    variant: {
+      bottom: "dropdown-bottom",
+      bottomEnd: "dropdown-bottom dropdown-end",
+      top: "dropdown-top",
+      topEnd: "dropdown-top dropdown-end",
+      left: "dropdown-left",
+      leftEnd: "dropdown-left dropdown-end",
+      right: "dropdown-right",
+      rightEnd: "dropdown-right dropdown-end"
+    }
+  },
+  defaultVariants: {
+    variant: "bottom"
+  }
+})
+
+const DropDown = (props: dropdownProps) => {
+  const { children, variant, className, trigger, ...otherProps } = props
+  const detailsRef = useRef<HTMLDetailsElement>(null)
+
+  const onAwayClick = () => {
+    detailsRef?.current?.removeAttribute("open")
+  }
+
+  const onMouseEnter = () => {
+    if (trigger !== "hover") return
+    detailsRef.current?.setAttribute("open", "")
+  }
+
+  const onMouseLeave = () => {
+    if (trigger !== "hover") return
+    detailsRef.current?.removeAttribute("open")
+  }
+
+  return (
+    <ClickAwayListener onClickAway={onAwayClick}>
+      <details
+        ref={detailsRef}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        className={cn(dropdownVariants({ variant, className }))}
+        {...otherProps}
+      >
+        <DropdownContext.Provider value={detailsRef}>
+          {children}
+        </DropdownContext.Provider>
+      </details>
+    </ClickAwayListener>
+  )
+}
+
+export default DropDown
